a1d2c697f5d8b0832f0fe58cd63d7f948cf40ccc
Ziawen <38622250+Ziawen@users.noreply.github.com>
2020-08-06 22:24:21 +0300

    nodemcu-rgbled, v3 update
    Updated sync system,
    Fixed some issues,
    Performance improvement

--- main.c
+++ main.c
@@ -5,247 +5,275 @@
 #define LED_GREEN 12
 #define LED_BLUE 13
 
-//Blynk variable inputs - start
-int LED_PROFILE,LED_MODE,BLYNK_RED,BLYNK_GREEN,BLYNK_BLUE;
+//Blynk variable inputs
+int POWER, LED_PROFILE, LED_MODE, BLYNK_RED, BLYNK_GREEN, BLYNK_BLUE;
 int animdelay = 1;
-//Blynk variable inputs - end
 
 
-//Auth Blynk - start
-char auth[] = "auth";
 
-char ssid[] = "ssid";
-char pass[] = "pass";
-//Auth Blynk - end
+//Auth Blynk
+char auth[] = "bxY96_vZYUoiW5kUyehyV8Ou8XOk5VOx";
+char ssid[] = "SEBILCI";
+char pass[] = "a8fe7bc4";
 
 
+BlynkTimer timer;
 void setup()
 {
-  // Debug console & Leds mode inputs - start
+  // Debug console & Leds mode inputs & DataSync - start
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
-  // Debug console & Leds mode inputs - end
+  pinMode(2, OUTPUT);
+  digitalWrite(2, 1);
+  timer.setInterval(1000L, sensorDataSend); //timer will run every sec
 }
 
-//Sync Blynk Signals - start
+//Sync Data
+void sensorDataSend()
+{
+  Blynk.syncVirtual(V10, V11, V12, V3, V7, V5, V4);
+}
 
-BLYNK_WRITE(V10) 
+
+
+
+////////////////Sync Blynk Signals
+BLYNK_WRITE(V10)
 {
   BLYNK_RED = param.asInt();
-  Serial.println(BLYNK_RED);
-  
-}
+  Serial.print("Red = ");
+  Serial.print(BLYNK_RED);
 
-BLYNK_WRITE(V7) 
-{
-  animdelay = param.asInt();
-  Serial.println(animdelay);
-  
 }
-
-BLYNK_WRITE(V11) 
+BLYNK_WRITE(V11)
 {
   BLYNK_GREEN = param.asInt();
-  Serial.println(BLYNK_GREEN);
-  
-}
+  Serial.print(" Green = ");
+  Serial.print(BLYNK_GREEN);
 
-BLYNK_WRITE(V12) 
+}
+BLYNK_WRITE(V12)
 {
   BLYNK_BLUE = param.asInt();
+  Serial.print(" Blue = ");
   Serial.println(BLYNK_BLUE);
-  
-}
 
-BLYNK_WRITE(V5) 
+}
+BLYNK_WRITE(V3)
+{
+  POWER = param.asInt();
+  Serial.print("Power = ");
+  Serial.println(POWER);
+}
+BLYNK_WRITE(V7)
+{
+  animdelay = param.asInt();
+  Serial.print("Delay = ");
+  Serial.println(animdelay);
+
+}
+BLYNK_WRITE(V5)
 {
   LED_MODE = param.asInt();
+  Serial.print("Mode = ");
   Serial.println(LED_MODE);
-  
-}
 
+}
 BLYNK_WRITE(V4)
 {
   LED_PROFILE = param.asInt();
+  Serial.print("Profile = ");
   Serial.println(LED_PROFILE);
-  
+  Serial.println(" ");
+  Serial.println(" ");
+
 }
+////////////////Sync Blynk Signals - End
 
 
 
+////////////////Effect Voids
 
 
-
-//Sync Blynk Signals - end
-
-
-
-void ledkapat(){ 
-        analogWrite(LED_RED, 0);
-        analogWrite(LED_GREEN, 0);
-        analogWrite(LED_BLUE, 0);
-  }
+void ledkapat() {
+  analogWrite(LED_RED, 0);
+  analogWrite(LED_GREEN, 0);
+  analogWrite(LED_BLUE, 0);
+}
 
 
 //Rgbwave effect - start
 
-void rgbwave(){
-   for(int i=0; i<255; i++){
-        analogWrite(LED_RED, 255);
-        analogWrite(LED_GREEN, 0);
-        analogWrite(LED_BLUE, i);
-        delay(animdelay);
-        }
-        
-        Blynk.syncVirtual(V10,V11,V12,V5,V4);
-        
-       for(int i=255; i>0; i--){
-        analogWrite(LED_RED, i);
-        delay(animdelay);
-        }
-        Blynk.syncVirtual(V10,V11,V12,V5,V4);
-        
-       for(int i=0; i<255; i++){
-        analogWrite(LED_RED, 0);
-        analogWrite(LED_GREEN, i);
-        analogWrite(LED_BLUE, 255);
-        delay(animdelay);
-        }
-        Blynk.syncVirtual(V10,V11,V12,V5,V4);
-       for(int i=255; i>0; i--){
-        analogWrite(LED_BLUE, i);
-        delay(animdelay);
-        }
-         Blynk.syncVirtual(V10,V11,V12,V5,V4);
-       for(int i=0; i<255; i++){
-        analogWrite(LED_RED, i);
-        analogWrite(LED_GREEN, 255);
-        analogWrite(LED_BLUE, 0);
-        delay(animdelay);
-       
-        }
-         Blynk.syncVirtual(V10,V11,V12,V5,V4);
-       for(int i=255; i>0; i--){
-        analogWrite(LED_GREEN, i);
-        delay(animdelay);
-        
-        }
-       Blynk.syncVirtual(V10,V11,V12,V5,V4);
+void rgbwave() {
+  for (int i = 0; i < 255; i++) {
+    analogWrite(LED_RED, 255);
+    analogWrite(LED_GREEN, 0);
+    analogWrite(LED_BLUE, i);
+    delay(animdelay);
   }
+
+
+
+  for (int i = 255; i > 0; i--) {
+    analogWrite(LED_RED, i);
+    delay(animdelay);
+  }
+
+
+  for (int i = 0; i < 255; i++) {
+    analogWrite(LED_RED, 0);
+    analogWrite(LED_GREEN, i);
+    analogWrite(LED_BLUE, 255);
+    delay(animdelay);
+  }
+
+  for (int i = 255; i > 0; i--) {
+    analogWrite(LED_BLUE, i);
+    delay(animdelay);
+  }
+
+  for (int i = 0; i < 255; i++) {
+    analogWrite(LED_RED, i);
+    analogWrite(LED_GREEN, 255);
+    analogWrite(LED_BLUE, 0);
+    delay(animdelay);
+
+  }
+
+  for (int i = 255; i > 0; i--) {
+    analogWrite(LED_GREEN, i);
+    delay(animdelay);
+
+  }
+
+}
 
 //Rgbwave effect - end
 
 
-void hearteffect1(){
-for(int i=25; i<255; i++){
-        analogWrite(LED_RED, i);
-        delay(2);
-        }
-  Blynk.syncVirtual(V10,V11,V12,V5,V4);
-  
-  for(int i=255; i>25; i--){
-        analogWrite(LED_RED, i);
-        delay(2);
-        }
-      Blynk.syncVirtual(V10,V11,V12,V5,V4);
+void hearteffect1() {
+  for (int i = 25; i < 255; i++) {
+    analogWrite(LED_RED, i);
+    delay(2);
+  }
+
+
+  for (int i = 255; i > 25; i--) {
+    analogWrite(LED_RED, i);
+    delay(2);
+  }
+
 }
 
 
 
 
 //heart effect - start
-void hearteffect(){
-      Blynk.syncVirtual(V10,V11,V12,V5,V4);
-      analogWrite(LED_GREEN, 0);
-      analogWrite(LED_BLUE, 0);
-        
-      hearteffect1();
-
-        
-      
-      hearteffect1();
-
-  
-        delay(500);
-  
-      hearteffect1();
-        
-      
-      hearteffect1();
-        delay(500);
+void hearteffect() {
+  analogWrite(LED_GREEN, 0);
+  analogWrite(LED_BLUE, 0);
+  hearteffect1();
+  hearteffect1();
+  delay(500);
+  hearteffect1();
+  hearteffect1();
+  delay(500);
 }
 
-//heart effect - end
 
 
 
 //randomlights - start
-void randomlights(){
-    int profil3delay;
-        if(animdelay<4){profil3delay =4*10;}else {profil3delay = animdelay*10;}
-       
-        Blynk.syncVirtual(V10,V11,V12,V5,V4);
-        analogWrite(LED_GREEN, 0);
-        analogWrite(LED_BLUE, 0);
-        analogWrite(LED_RED, 255);
-        delay(profil3delay);
-        analogWrite(LED_GREEN, 0);
-        analogWrite(LED_BLUE, 0);
-        analogWrite(LED_RED, 0);
-        delay(profil3delay);
-        Blynk.syncVirtual(V10,V11,V12,V5,V4);
-        analogWrite(LED_RED, 0);
-        analogWrite(LED_BLUE, 0);
-        analogWrite(LED_GREEN, 255);
-        delay(profil3delay);
-        analogWrite(LED_GREEN, 0);
-        analogWrite(LED_BLUE, 0);
-        analogWrite(LED_RED, 0);
-        delay(profil3delay);
-        Blynk.syncVirtual(V10,V11,V12,V5,V4);
-        analogWrite(LED_RED, 0);
-        analogWrite(LED_GREEN, 0);
-        analogWrite(LED_BLUE, 255);
-        delay(profil3delay);
-        ledkapat();
-        delay(profil3delay);
+void randomlights() {
+  int profil3delay;
+  if (animdelay < 4) {
+    profil3delay = 4 * 10;
+  } else {
+    profil3delay = animdelay * 10;
   }
-//randomlights - end
 
 
+  analogWrite(LED_GREEN, 0);
+  analogWrite(LED_BLUE, 0);
+  analogWrite(LED_RED, 255);
 
-void loop(){
-  Blynk.syncVirtual(V10,V11,V12,V5,V4);
-  if(LED_MODE == 1){
+  delay(profil3delay);
+  ledkapat();
+  delay(profil3delay);
+
+  analogWrite(LED_RED, 0);
+  analogWrite(LED_BLUE, 0);
+  analogWrite(LED_GREEN, 255);
+
+  delay(profil3delay);
+  ledkapat();
+  delay(profil3delay);
+
+  analogWrite(LED_RED, 0);
+  analogWrite(LED_GREEN, 0);
+  analogWrite(LED_BLUE, 255);
+
+  delay(profil3delay);
+  ledkapat();
+  delay(profil3delay);
+}
 
 
+//blink color effect - start
+void blinkcolor() {
+
+  int profile4delay;
+  profile4delay = animdelay * 50;
+  analogWrite(LED_RED, BLYNK_RED);
+  analogWrite(LED_GREEN, BLYNK_GREEN);
+  analogWrite(LED_BLUE, BLYNK_BLUE);
+  delay(profile4delay);
+  ledkapat();
+  delay(profile4delay);
+}
 
 
+////////////////Effect Voids - End
 
-    if(LED_PROFILE == 1){
-      
-      ledkapat();
-      rgbwave();
-      
-      }else if(LED_PROFILE == 2){
-       
-      hearteffect();
-        
-      }else if(LED_PROFILE == 3){
-
-      randomlights();
-        
-      }
-    
-
-    }else{
-    analogWrite(LED_RED, BLYNK_RED);
-    analogWrite(LED_GREEN, BLYNK_GREEN);
-    analogWrite(LED_BLUE, BLYNK_BLUE);
-      }
-    
+void loop() {
   Blynk.run();
+  timer.run();
+  if (POWER == 0) {
+    ledkapat();
+  } else {
+
+    if (LED_MODE == 1) {
+
+      if (LED_PROFILE == 1) {
+
+        ledkapat();
+        rgbwave();
+
+      } else if (LED_PROFILE == 2) {
+
+        hearteffect();
+
+      } else if (LED_PROFILE == 3) {
+
+        randomlights();
+
+      } else if (LED_PROFILE == 4) {
+
+        blinkcolor();
+
+      }
+
+
+    } else {
+      analogWrite(LED_RED, BLYNK_RED);
+      analogWrite(LED_GREEN, BLYNK_GREEN);
+      analogWrite(LED_BLUE, BLYNK_BLUE);
+    }
+
+  }
+
+
 }
